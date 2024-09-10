368c12ffd078714089325e907049c6f099b97f9e
Hikaru-Katani <hkatani3@gatech.edu>
2024-09-10 01:15:29 +0000

    Modify the file contents to adjust to protein against DNA

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,10 +2,11 @@
 
 query="$1"
 subject="$2"
+outputfile="$3"
 
-blastn -query "$query" -subject "$subject" -task blastn-short -outfmt "6 sseq sseqid pident length" -out blast_output.txt
+tblastn -query "$query" -subject "$subject" -task blastn-short -outfmt "6 sseq sseqid pident length qlen" -out blast_output.txt
 
-# Filter for perfect matches (100% identity and equal length)
-awk -v OFS="\t" '$3 == 100 && $4 == 28 { print $0 }' blast_output.txt > output.txt
+# Filter
+awk -v OFS="\t" '$3 > 30 && $4 / $5 > 0.9 { print $0 }' blast_output.txt > output.txt
 
 cat output.txt | wc -l
