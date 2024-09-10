620f7847d810caa91bfc505bdd9154326a7c1b64
Hikaru-Katani <hkatani3@gatech.edu>
2024-09-10 01:00:51 +0000

    modified

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -1,1 +1,11 @@
-Oops!
+#!/bin/bash
+
+query="$1"
+subject="$2"
+
+blastn -query "$query" -subject "$subject" -task blastn-short -outfmt "6 sseq sseqid pident length" -out blast_output.txt
+
+# Filter for perfect matches (100% identity and equal length)
+awk -v OFS="\t" '$3 == 100 && $4 == 28 { print $0 }' blast_output.txt > output.txt
+
+cat output.txt | wc -l
