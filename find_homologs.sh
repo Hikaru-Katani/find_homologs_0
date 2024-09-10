1c5bb755c0b69ab8a0459c964a8d09015a320126
Hikaru-Katani <hkatani3@gatech.edu>
2024-09-10 01:20:17 +0000

    Modify - outfile

--- find_homologs.sh
+++ find_homologs.sh
@@ -7,6 +7,6 @@ outputfile="$3"
 tblastn -query "$query" -subject "$subject" -task blastn-short -outfmt "6 sseq sseqid pident length qlen" -out blast_output.txt
 
 # Filter
-awk -v OFS="\t" '$3 > 30 && $4 / $5 > 0.9 { print $0 }' blast_output.txt > output.txt
+awk -v OFS="\t" '$3 > 30 && $4 / $5 > 0.9 { print $0 }' blast_output.txt > "%$outputfile"
 
-cat output.txt | wc -l
+cat "$outputfile" | wc -l
